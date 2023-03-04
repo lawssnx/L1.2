e63e5c513c2099f3752a59e8a9506a3c1f621035
anastasi <120751391+lawssnx@users.noreply.github.com>
2023-03-04 18:46:35 +0200

    Update L1.2.cs

--- L1.2.cs
+++ L1.2.cs
@@ -1,8 +1,29 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-public class Class1
+class Program
 {
-	public Class1()
-	{
-	}
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> dictionary1 = new Dictionary<string, int>()
+        {
+            { "key1", 1 },
+            { "key2", 2 },
+            { "key3", 3 },
+        };
+        Dictionary<string, int> dictionary2 = new Dictionary<string, int>()
+        {
+            {"key1", 1 },
+            {"key2", 2 },
+        };
+        var equalKeys = dictionary1.Keys.Intersect(dictionary2.Keys);
+        var equalValues = from key in equalKeys
+                          where dictionary1[key] == dictionary2[key]
+                          select new {Key = key, Value = dictionary1[key]};
+        foreach (var equals in equalValues)
+        {
+            Console.WriteLine("{0}: {1} is present in both x and y", equals.Key, equals.Value);
+        }
+
+    }
 }
